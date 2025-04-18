54ec41aa2e0c52b7c1206106914f9d051503661c
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-04-18 18:22:31 +0530

    Altas added

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose")
 const config = require("config");
 
-// mongoose.connect(`${config.get("MONGODB_URI")}/food_delivery`)
-//     .then(() => {
-//        console.log("MongoDB connected successfully!");
-//     })
-//     .catch((error) => {
-//         console.log("MongoDB connection error:", error);
-//     });
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("Connection error:", err));
+
+
+
 
 
 module.exports = mongoose.connection;

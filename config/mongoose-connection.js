d5b151b6092a2154bbdae76554a5db832edfefd9
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-04-18 18:12:18 +0530

    DB added

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose")
 const config = require("config");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/food_delivery`)
-    .then(() => {
-       console.log("MongoDB connected successfully!");
-    })
-    .catch((error) => {
-        console.log("MongoDB connection error:", error);
-    });
+// mongoose.connect(`${config.get("MONGODB_URI")}/food_delivery`)
+//     .then(() => {
+//        console.log("MongoDB connected successfully!");
+//     })
+//     .catch((error) => {
+//         console.log("MongoDB connection error:", error);
+//     });
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 
 module.exports = mongoose.connection;

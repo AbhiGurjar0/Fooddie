5710523602ae5fd66600b47ef221ce0979ec1df7
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-04-20 00:21:46 +0530

    altas added

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose")
 const config = require("config");
-mongoose.connect(`${config.get("MONGODB_URI")}/food_delivery`)
-.then(()=>{
-    console.log("MongoDB Connected");
-})
-.catch(err =>{
-    console.error("Connection error:", err);
-})
+// mongoose.connect(`${config.get("MONGODB_URI")}/food_delivery`)
+// .then(()=>{
+//     console.log("MongoDB Connected");
+// })
+// .catch(err =>{
+//     console.error("Connection error:", err);
+// })
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch(err => console.error("Connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("Connection error:", err));
 
 
 module.exports = mongoose.connection;
